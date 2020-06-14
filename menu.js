81a4793c593590e0f3c2a640f9b2c9011e0e6684
Maxim <maxim07092001@gmail.com>
2020-06-14 19:57:03 +0300

    first commit

---
+++ menu.js
@@ -0,0 +1,11 @@
+/*function openMenu(){
+    document.getElementById("sidebar").classList.toggle('active');
+    
+}*/
+
+let btn = document.getElementById("sidebar");
+
+btn.addEventListener('click', function(){
+btn.classList.toggle('active');
+
+});
